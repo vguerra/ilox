17be148ae315442156dff2fbf9ba059ddf76dfd6
Victor Guerra <vguerra@gmail.com>
2021-09-27 23:23:58 +0200

    [NFC] Removing redundant definition of `Binary` expr.

--- Sources/ilox/Expression.swift
+++ Sources/ilox/Expression.swift
@@ -5,19 +5,3 @@
 //  Created by Victor Manuel Guerra Moran on 16/09/2021.
 //
 
-class Expression {
-
-}
-
-final class Binary : Expression {
-    let left: Expression
-    let right: Expression
-    let op: Token
-
-    init(_ left:Expression, _ op:Token, _ r ight: Expression) {
-        self.left = left
-        self.right = right
-        self.op = op
-        super.init()
-    }
-}
