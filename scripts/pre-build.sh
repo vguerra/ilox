3ec35f00bdd3b51757a8a3a62515d392a39eb3f5
Victor Guerra <vguerra@gmail.com>
2021-09-30 23:16:13 +0200

    [CI] Add bash script intended for pre-build to trigger code generation phase.
    We use sourcery to automatically generate the classes that represent
    AST Nodes, so we provide a script that executes sourcery. This script
    can be manually executed and we include it in the github workflow
    for the CI.

---
+++ scripts/pre-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+AST_SOURCERY_CONFIG="${SCRIPT_DIR}/../Sources/ilox/AST/.sourcery.yml"
+sourcery --config $AST_SOURCERY_CONFIG
