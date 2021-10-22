4a532be8ca79471f2a4f75ae9ecb8df9310fcc78
Victor Guerra <vguerra@gmail.com>
2021-10-22 23:22:31 +0200

    [Parser] Rename sourcery config file so that it is not hidden.

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-AST_SOURCERY_CONFIG="${SCRIPT_DIR}/../Sources/ilox/AST/.sourcery.yml"
+AST_SOURCERY_CONFIG="${SCRIPT_DIR}/../Sources/ilox/AST/expressions.sourcery.yml"
 sourcery --config $AST_SOURCERY_CONFIG
